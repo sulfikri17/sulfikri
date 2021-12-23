3c8d7568bf9f6e3936c12030b2eec188ed14e731
sulfikri17 <sulfikri017@gmail.com>
2021-12-23 04:34:51 -0800

    program wab

---
+++ bioadata.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+      <html>
+     <head>
+      <title> Penggunaan Tag Tabel</title>
+          </head>
+      <body>
+      <table border = "3" style = background-color:aqua>
+      <tr>
+      <td style = background-color:blue colspan = "3"><center><b><font color = white size = 30 >Daftar Mahasiswa</font></b></center>  </td>
+  </tr>
+  <tr>
+  <td style = background-color:blue colspan = "3"><center><b><font color = white size = 30 > Politeknik Elektronika Negeri Surabaya
+</font></b></center></td>
+  </tr>
+ <tr>
+  <td>Nrp</td>
+  <td>17024014013</td>
+  <td rowspan =25><center><img src = "tt.jpg" width = 200 high = 300/></center></td>
+  </tr>
+  <tr>
+<td>Nama</td>
+<td>SULFIKRI </td>
+ </tr>
+  <tr>
+<td>Program</td>
+<td>S1</td>
+  </tr>
+  <tr>
+<td>Jurusan</td>
+<td>Teknik INFORMATIKA </td>
+  </tr>
+  <tr>
+<td>Kelas</td>
+<td>A1</td>
+ </tr>
+  <tr>
+<td>Tangga Lahir </td>
+<td>09 FEBRUARI 1999</td>
+ </tr>
+  <tr>
+<td>Jenis Kelamin</td>
+<td>Laki-Laki</td>
+  </tr>
+  <tr>
+<td>Warga </td>
+<td>WNI</td>
+ </tr>
+  <tr>
+<td>Agama</td>
+<td>Islam</td>  </tr>
+ <tr>
+<td>Golongan Darah</td>
+<td>O</td>
+  </tr>
+  <tr>
+<td>Alamat</td>
+<td>JL,DR,J LAIMENA</td>
+  </tr>
+
+</table>
+  </body>
+  </html>
